76bc03fc9c39388f8d8f9ae2012ca1378b9c80eb
karinmia <karinmia99avr@gmail.com>
2019-08-21 14:51:11 +0300

    Fix Shop app

--- shop/views.py
+++ shop/views.py
@@ -1,46 +1,38 @@
 import logging
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.schemas import SchemaGenerator
+from rest_framework.views import APIView
+from rest_framework.pagination import LimitOffsetPagination
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
 from .models import Book
+from .serializers import *
 from .forms import BookForm
 
 
-def book_list(request):
-    books = Book.objects.all()
+class BookList(APIView):
+    """
+    Get list of all currencies
+    """
+    serializer_class = BookSerializer
 
-    if "new-first" in request.GET:
-        books = books.order_by('-publish_date')
-    elif "old-first" in request.GET:
-        books = books.order_by('publish_date')
-
-    return render(request, 'shop/book_list.html', {'books': books})
-
-
-def book_details(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, 'shop/book_details.html', {'book': book})
-
-
-def book_edit(request, pk=None):
-    book = get_object_or_404(Book, pk=pk) if pk else None
-
-    if request.method == "POST":
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            book = form.save(commit=False)
-            book.save()
-            return redirect('book_details', pk=book.pk)
-    else:
-        form = BookForm(instance=book)
-
-    if pk:
-        action = reverse(book_edit, args=[pk])
-    else:
-        action = reverse(book_edit)
-
-    return render(request, 'shop/book_edit.html', {'form': form, 'action': action})
+    @swagger_auto_schema(
+        operation_id='Get books',
+        security=[],
+        tags=['Book'],
+    )
+    def get(self, request):
+        books = Book.objects.all().order_by('-publish_date')
+        data = self.serializer_class(books, many=True).data
+        return Response({'books': data}, status=status.HTTP_200_OK)
 
 
 def book_delete(request, pk):
